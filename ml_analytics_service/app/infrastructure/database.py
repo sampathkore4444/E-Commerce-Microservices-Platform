fedc86c78f93add7ab7f9a2b6150a7b9a4a8cfd5
Kore Sampath Kumar <sampathkore4444@gmail.comr>
2026-02-13 17:29:26 +0700

    added modified database.py in some services as copied blindly before

--- ml_analytics_service/app/infrastructure/database.py
+++ ml_analytics_service/app/infrastructure/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DB_USER = os.getenv("ORDER_DB_USER", "postgres")
-DB_PASSWORD = os.getenv("ORDER_DB_PASSWORD", "postgres")
-DB_HOST = os.getenv("ORDER_DB_HOST", "localhost")
-DB_PORT = os.getenv("ORDER_DB_PORT", "5432")
-DB_NAME = os.getenv("ORDER_DB_NAME", "order_db")
+DB_USER = os.getenv("ML_DB_USER", "postgres")
+DB_PASSWORD = os.getenv("ML_DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("ML_DB_HOST", "localhost")
+DB_PORT = os.getenv("ML_DB_PORT", "5432")
+DB_NAME = os.getenv("ML_DB_NAME", "ml_db")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20)
--- shipping_service/app/infrastructure/database.py
+++ shipping_service/app/infrastructure/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DB_USER = os.getenv("PRODUCT_DB_USER", "postgres")
-DB_PASSWORD = os.getenv("PRODUCT_DB_PASSWORD", "postgres")
-DB_HOST = os.getenv("PRODUCT_DB_HOST", "localhost")
-DB_PORT = os.getenv("PRODUCT_DB_PORT", "5432")
-DB_NAME = os.getenv("PRODUCT_DB_NAME", "product_db")
+DB_USER = os.getenv("SHIPPING_DB_USER", "postgres")
+DB_PASSWORD = os.getenv("SHIPPING_DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("SHIPPING_DB_HOST", "localhost")
+DB_PORT = os.getenv("SHIPPING_DB_PORT", "5432")
+DB_NAME = os.getenv("SHIPPING_DB_NAME", "shipping_db")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20)
